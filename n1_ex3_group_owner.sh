130b086919bf9c4ff74c292b85afc27e970bfd48
meamuri <meammf@yandex.ru>
2017-10-21 20:05:06 +0300

    add user variable

--- n1_ex3_group_owner.sh
+++ n1_ex3_group_owner.sh
@@ -6,9 +6,12 @@ USAGE="Usage: n1_ex3_group_owner [--help]"
 INFO="Найти в директории файлы, группа-владелец которых содержит \n
 текущего пользователя и являются обычными файлами"
 
-if [$1 == '--help' ]]; then
+if [[ $# >0 && $1 -eq '--help' ]]; then
 	echo $USAGE
 	echo $INFO
 else
-	find . -type f -print
+    u="$USER"
+	# find . -type f -gname -print
+    echo $u
+    find . -type f -print
 fi
