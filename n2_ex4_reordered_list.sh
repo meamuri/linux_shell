8be6c136c6eb43b6799c710127089a849859ded9
meamuri <meammf@yandex.ru>
2017-10-25 00:22:11 +0300

    empty output of second version of second lab

--- n2_ex4_reordered_list.sh
+++ n2_ex4_reordered_list.sh
@@ -4,5 +4,7 @@
 # полночи до полночи, независимо от текущего часа и минуты)
 
 last -t "-1 day" '+%Y-%m-%d' | awk '{print $1;}' | sort -r | uniq
-# date -d "-1 day" '+%Y-%m-%d'
-# last -t "yesterday" '+%Y-%m-%d' | sort -r | uniq
+
+echo "   "
+
+last | grep "`date -d "-2 day" | awk '{print $1 " " $2 " " $3 }'`"
