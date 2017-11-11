df100752eb3f1482c23304bd02a7fe3732eb74d8
meamuri <meammf@yandex.ru>
2017-11-11 15:10:03 +0300

    awk invoke correct

--- n4_ex2_tex.sh
+++ n4_ex2_tex.sh
@@ -9,7 +9,7 @@
 # AWK.
 
 USAGE="
-Usage: n4_ex2_tex output
+Usage: n4_ex2_tex
 Task: Вывести список пользователей, присутствующих в файле /etc/group, список уникален, 
 отсортирован, для каждого вывести количество повторений"
 
@@ -18,52 +18,23 @@ HEADER="
 \\usepackage[english]{babel}
 \\usepackage{longtable}
 \\begin{document}
-\\begin{longtable}{ |l|l| }
-\\hline 
-\\multicolumn{1}{|c|}{User} &
-\\multicolumn{1}{c|}{Count} \\\\
-\\endfirsthead
-\\hline
-\\endhead
-\\hline
-\\endfoot
 "
 
-AWK_CODE='
-{
-  depth[NR] = $1
-  nfs[NR] = NF
-  for (i = 2; i <= NF; i++)
-    a[NR, i - 1] = $i
-}
-END {
-  for (i = 1; i <= NR; i++) {
-    printf "\\hline " depth[i] "\n"
-    for (j = 1; j < nfs[i]; j++) {
-      printf " & " a[i, j] " \\\\ \n"
-    }
-  }
-}
-'
-
-
 TAIL="
-\\end{longtable}
 \\end{document}
 "
 
 
-if [ "$1" == "-h" ] || [[ $# != 1 ]]; then
+if [ "$1" == "-h" ] || [[ $# != 0 ]]; then
     echo "$USAGE"
     exit 0
 fi
 
 OUT="res.pdf"
 INFO=$(cat /etc/group | grep "[a-z,]\+$" -o | tr ',' '\n' | sort | uniq -c)
-echo $INFO
 TMP=`mktemp`
 echo "$HEADER" > "$TMP"
-# awk "$AWK_CODE" "$IN" >> "$TMP"
+awk -v body="$INFO" 'BEGIN{printf body}' >> "$TMP"
 echo "$TAIL" >> "$TMP"
 pdflatex "$TMP" > /dev/null
 rm "$TMP"
